bc38d0ec9063893fcad631e3287af092a1b28336
celsisus <ezrasussman@gmail.com>
2021-06-23 09:30:32 -0400

    added spawn egg and lang for enderman brute

--- src/main/java/com/ejs/endplus/registry/ModItems.java
+++ src/main/java/com/ejs/endplus/registry/ModItems.java
@@ -4,13 +4,14 @@ import com.ejs.endplus.EndPlus;
 
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.SpawnEggItem;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModItems {
-	public static final Item TEST = new Item(new Item.Settings().group(ItemGroup.MISC));
+	public static final SpawnEggItem TEST = new SpawnEggItem(EndPlus.ENDERMAN_BRUTE, 27923100, 27910024, new Item.Settings().group(ItemGroup.MISC));
 	
 	public static void registerItems() {
-		Registry.register(Registry.ITEM, new Identifier(EndPlus.MOD_ID, "test"), TEST);
+		Registry.register(Registry.ITEM, new Identifier(EndPlus.MOD_ID, "ender_brute_spawn_egg"), TEST);
 	}
 }
